2c9f056f49b8af3beef365b352a71324cdf45046
risaadodx <wrisaad@gmail.com>
2022-10-11 13:30:26 +0600

    created components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
+import { createBrowserRouter RouterProvider} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([]);
   return (
     <div className="App">
+      <RouterProvider router={router}></RouterProvider>
       <Header></Header>
     </div>
   );
